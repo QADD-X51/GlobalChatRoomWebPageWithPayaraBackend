4a83011e2c88e368e22204bebb6cc7d2417ac272
Alex31213 <alex.snd01@gmail.com>
2024-01-16 13:25:21 +0200

    Frontend WebPage
    Added frontend for the Payara WebApplication.

---
+++ Frontend PayaraWebApplication/wwwroot/js/lobbyChat.js
@@ -0,0 +1,260 @@
+const baseUrl = "http://localhost:8080/PayaraWebApplication-1.0-SNAPSHOT/main";
+const baseAddressUrl = window.location.href.replace(/\/[^\/]*$/, "/");
+let sendChatTextButton = document.getElementById('sendChatText');
+let logoutButton = document.getElementById('logoutButton');
+var lastFormattedDate = "";
+
+function padZero(num) {
+    return num < 10 ? "0" + num : num;
+}
+
+function getAllMessages() {
+    const getAllURL = `${baseUrl}/message/all`;
+    fetch(getAllURL, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.json().then(error => { throw error; });
+            }
+        })
+        .then(data => {
+            console.log(data);
+            const messageChatContainer = document.getElementById('message-chatContainer');
+            messageChatContainer.innerHTML = '';
+            data.forEach(function (messageEntry) {
+                var messageContainer = document.createElement('div');
+                messageContainer.className = 'messageContainer';
+
+                if (messageEntry.username === sessionStorage.getItem("username")) {
+                    messageContainer.style.backgroundColor = "#4682b4";
+                }
+
+                var bubble = document.createElement('div');
+                bubble.className = 'bubble';
+
+                var shortUsernameSpan = document.createElement('span');
+                shortUsernameSpan.className = 'shortUsername';
+                shortUsernameSpan.textContent = messageEntry.username.slice(0, 2);
+
+                var fullUsernameSpan = document.createElement('span');
+                fullUsernameSpan.className = 'fullUsername';
+                fullUsernameSpan.textContent = messageEntry.username;
+
+                bubble.appendChild(shortUsernameSpan);
+                bubble.appendChild(fullUsernameSpan);
+
+                var spanElement = document.createElement('span');
+                spanElement.className = 'messageChat';
+                spanElement.textContent = messageEntry.message;
+
+                let aux = shortUsernameSpan.textContent;
+
+                bubble.addEventListener('mouseenter', function () {
+                    shortUsernameSpan.textContent = fullUsernameSpan.textContent;
+                });
+
+                bubble.addEventListener('mouseleave', function () {
+                    shortUsernameSpan.textContent = aux;
+                });
+
+                var dateString = messageEntry.date;
+
+                var dateObject = new Date(dateString);
+
+                var year = dateObject.getFullYear();
+                var month = dateObject.getMonth() + 1;
+                var day = dateObject.getDate();
+                var hour = dateObject.getHours();
+                var minute = dateObject.getMinutes();
+
+                var formattedTime = padZero(hour) + ":" + padZero(minute);
+                var formattedDateTime = year + "/" + padZero(month) + "/" + padZero(day) + " " + padZero(hour) + ":" + padZero(minute);
+
+                var bubbleData = document.createElement('div');
+                bubbleData.className = 'bubble';
+
+                var shortDataSpan = document.createElement('span');
+                shortDataSpan.className = 'shortUsername';
+                shortDataSpan.textContent = formattedTime;
+
+                var fullDataSpan = document.createElement('span');
+                fullDataSpan.className = 'fullUsername';
+                fullDataSpan.textContent = formattedDateTime;
+
+                bubbleData.appendChild(shortDataSpan);
+                bubbleData.appendChild(fullDataSpan);
+
+                let auxData = shortDataSpan.textContent;
+
+                bubbleData.addEventListener('mouseenter', function () {
+                    shortDataSpan.textContent = fullDataSpan.textContent;
+                });
+
+                bubbleData.addEventListener('mouseleave', function () {
+                    shortDataSpan.textContent = auxData;
+                });
+
+                messageContainer.appendChild(bubble);
+                messageContainer.appendChild(spanElement);
+                messageContainer.appendChild(bubbleData);
+
+                messageChatContainer.appendChild(messageContainer);
+            });
+            var currentDate = new Date();
+            lastFormattedDate = currentDate.getFullYear() + '-' +
+                padZero(currentDate.getMonth() + 1) + '-' +
+                padZero(currentDate.getDate()) + 'T' +
+                padZero(currentDate.getHours()) + ':' +
+                padZero(currentDate.getMinutes()) + ':' +
+                padZero(currentDate.getSeconds()) + '.' +
+                (currentDate.getMilliseconds() / 1000).toFixed(3).slice(2, 5);
+        });
+}
+
+function updateMessages() {
+    var currentDate = new Date();
+    const getAllURL = `${baseUrl}/message/${lastFormattedDate}`;
+    lastFormattedDate =
+        currentDate.getFullYear() + '-' +
+        padZero(currentDate.getMonth() + 1) + '-' +
+        padZero(currentDate.getDate()) + 'T' +
+        padZero(currentDate.getHours()) + ':' +
+        padZero(currentDate.getMinutes()) + ':' +
+        padZero(currentDate.getSeconds()) + '.' +
+        (currentDate.getMilliseconds() / 1000).toFixed(3).slice(2, 5);
+    fetch(getAllURL, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.json().then(error => { throw error; });
+            }
+        })
+        .then(data => {
+            const messageChatContainer = document.getElementById('message-chatContainer');
+            data.forEach(function (messageEntry) {
+                console.log(messageEntry);
+                var messageContainer = document.createElement('div');
+                messageContainer.className = 'messageContainer';
+
+                if (messageEntry.username === sessionStorage.getItem("username")) {
+                    messageContainer.style.backgroundColor = "#4682b4";
+                }
+
+                var bubble = document.createElement('div');
+                bubble.className = 'bubble';
+
+                var shortUsernameSpan = document.createElement('span');
+                shortUsernameSpan.className = 'shortUsername';
+                shortUsernameSpan.textContent = messageEntry.username.slice(0, 2);
+
+                var fullUsernameSpan = document.createElement('span');
+                fullUsernameSpan.className = 'fullUsername';
+                fullUsernameSpan.textContent = messageEntry.username;
+
+                bubble.appendChild(shortUsernameSpan);
+                bubble.appendChild(fullUsernameSpan);
+
+                var spanElement = document.createElement('span');
+                spanElement.className = 'messageChat';
+                spanElement.textContent = messageEntry.message;
+
+                let aux = shortUsernameSpan.textContent;
+
+                bubble.addEventListener('mouseenter', function () {
+                    shortUsernameSpan.textContent = fullUsernameSpan.textContent;
+                });
+
+                bubble.addEventListener('mouseleave', function () {
+                    shortUsernameSpan.textContent = aux;
+                });
+
+                var dateString = messageEntry.date;
+
+                var dateObject = new Date(dateString);
+
+                var year = dateObject.getFullYear();
+                var month = dateObject.getMonth() + 1;
+                var day = dateObject.getDate();
+                var hour = dateObject.getHours();
+                var minute = dateObject.getMinutes();
+
+                var formattedTime = padZero(hour) + ":" + padZero(minute);
+                var formattedDateTime = year + "/" + padZero(month) + "/" + padZero(day) + " " + padZero(hour) + ":" + padZero(minute);
+
+                var bubbleData = document.createElement('div');
+                bubbleData.className = 'bubble';
+
+                var shortDataSpan = document.createElement('span');
+                shortDataSpan.className = 'shortUsername';
+                shortDataSpan.textContent = formattedTime;
+
+                var fullDataSpan = document.createElement('span');
+                fullDataSpan.className = 'fullUsername';
+                fullDataSpan.textContent = formattedDateTime;
+
+                bubbleData.appendChild(shortDataSpan);
+                bubbleData.appendChild(fullDataSpan);
+
+                let auxData = shortDataSpan.textContent;
+
+                bubbleData.addEventListener('mouseenter', function () {
+                    shortDataSpan.textContent = fullDataSpan.textContent;
+                });
+
+                bubbleData.addEventListener('mouseleave', function () {
+                    shortDataSpan.textContent = auxData;
+                });
+
+                messageContainer.appendChild(bubble);
+                messageContainer.appendChild(spanElement);
+                messageContainer.appendChild(bubbleData);
+
+                messageChatContainer.appendChild(messageContainer);
+            });
+        });
+}
+
+setInterval(updateMessages, 1000);
+
+sendChatTextButton.onclick = function () {
+    let chatTextToSendField = document.getElementById('chatTextToSend');
+    var userText = chatTextToSendField.value;
+    if (userText.length > 0) {
+        const getAllURL = `${baseUrl}/message/add`;
+        fetch(getAllURL, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: userText,
+                senderId: sessionStorage.getItem("id"),
+            })
+        });
+        chatTextToSendField.value = '';
+    }
+}
+
+logoutButton.onclick = function () {
+    sessionStorage.removeItem("username");
+    sessionStorage.removeItem("id");
+    const redirectUrl = baseAddressUrl + "login.html";
+    window.location.replace(redirectUrl);
+}
+$(window).on('load', function () {
+    getAllMessages();
+    var loggedUsername = sessionStorage.getItem("username");
+    $('#usernameNavBar').text(loggedUsername);
+});
---
+++ Frontend PayaraWebApplication/wwwroot/js/login.js
@@ -0,0 +1,159 @@
+﻿const baseUrl = "http://localhost:8080/PayaraWebApplication-1.0-SNAPSHOT/main";
+const baseAddressUrl = window.location.href.replace(/\/[^\/]*$/, "/");
+const currentUrl = window.location.href;
+
+let loginButton = document.getElementById("loginButton");
+let registerButton = document.getElementById("registerButton");
+let usernameField = document.getElementById("usernameField");
+let passwordField = document.getElementById("passwordField");
+let confirmPasswordField = document.getElementById("confirmPasswordField");
+let title = document.getElementById("title");
+let errorText = document.getElementById("errorText");
+const asciiRegex = /^[a-zA-Z0-9_-]+$/;
+
+confirmPasswordField.style.maxHeight = "0px";
+
+loginButton.onclick = function () {
+    if (loginButton.classList.contains('disabled')) {
+        confirmPasswordField.style.maxHeight = "0px";
+        title.innerHTML = "Log In";
+        registerButton.classList.add("disabled");
+        loginButton.classList.remove("disabled");
+        errorText.innerHTML = " ";
+    }
+    else if (!loginButton.classList.contains('disabled')) {
+        if (usernameField.querySelector('input').value == "") {
+            errorText.innerHTML = "Username field is empty.";
+        }
+        else if (!isValidASCIIString(usernameField.querySelector('input').value)) {
+            errorText.innerHTML = "Username contains disallowed characters.";
+        }
+        else if (passwordField.querySelector('input').value == "") {
+            errorText.innerHTML = "Password field is empty.";
+        }
+        else {
+            const loginURL = `${baseUrl}/user/login/${usernameField.querySelector('input').value}/${passwordField.querySelector('input').value}`;
+            fetch(loginURL, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return response.json().then(error => { throw error; });
+                }
+            })
+            .then(data => {
+                console.log(data);
+                if (data.validationResult === "Ok") {
+                    sessionStorage.setItem("id", data.userId);
+                    sessionStorage.setItem("username", usernameField.querySelector('input').value);
+                    const redirectUrl = baseAddressUrl + "lobbyChat.html";
+                    window.location.replace(redirectUrl);
+                }
+                else {
+                    errorText.innerHTML = data.validationResult;
+                }
+                })
+            .catch(error => {
+                console.error(error);
+                errorText.innerHTML = error;
+            });
+        }
+    }
+}
+
+registerButton.onclick = function () {
+    if (registerButton.classList.contains('disabled')) {
+        confirmPasswordField.style.maxHeight = "65px";
+        title.innerHTML = "Register";
+        registerButton.classList.remove("disabled");
+        loginButton.classList.add("disabled");
+        errorText.innerHTML = " ";
+    }
+    else if (!registerButton.classList.contains('disabled')) {
+        if (passwordField.querySelector('input').value == confirmPasswordField.querySelector('input').value &&
+            passwordField.querySelector('input').value != "") {
+            const registerURL = `${baseUrl}/user/register`;
+            fetch(registerURL, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: usernameField.querySelector('input').value,
+                    password: passwordField.querySelector('input').value,
+                })
+            })
+                .then(response => {
+                    if (response.ok) {
+                        confirmPasswordField.style.maxHeight = "0px";
+                        title.innerHTML = "Log In";
+                        registerButton.classList.add("disabled");
+                        loginButton.classList.remove("disabled");
+                        errorText.innerHTML = " ";
+                        return response.text();
+                    } else {
+                        return response.text().then(error => { throw error; });
+                    }
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error(error);
+                    errorText.innerHTML = error;
+                });
+        }
+        else if (usernameField.querySelector('input').value == ""){
+            errorText.innerHTML = "Username field is empty.";
+        }
+        else if (passwordField.querySelector('input').value == "") {
+            errorText.innerHTML = "Password field is empty.";
+        }
+        else if (confirmPasswordField.querySelector('input').value == "") {
+            errorText.innerHTML = "Confirm Password field is empty.";
+        }
+        else if (passwordField.querySelector('input').value != confirmPasswordField.querySelector('input').value) {
+            errorText.innerHTML = "Passwords don't match. Try again.";
+        }
+    }
+}
+
+function isValidASCIIString(input) {
+    return asciiRegex.test(input);
+}
+
+usernameField.addEventListener("input", function () {
+    let inputElement = this.querySelector("input");
+    let inputValue = inputElement.value;
+
+    if (!isValidASCIIString(inputValue)) {
+        inputValue = inputValue.replace(/[^a-zA-Z0-9_-]/g, '');
+        inputElement.value = inputValue;
+    }
+});
+
+passwordField.addEventListener("input", function () {
+    let inputElement = this.querySelector("input");
+    let inputValue = inputElement.value;
+
+    if (!isValidASCIIString(inputValue)) {
+        inputValue = inputValue.replace(/[^a-zA-Z0-9_-]/g, '');
+        inputElement.value = inputValue;
+    }
+});
+
+confirmPasswordField.addEventListener("input", function () {
+    let inputElement = this.querySelector("input");
+    let inputValue = inputElement.value;
+
+    if (!isValidASCIIString(inputValue)) {
+        inputValue = inputValue.replace(/[^a-zA-Z0-9_-]/g, '');
+        inputElement.value = inputValue;
+    }
+});
+
